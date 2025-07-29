5a660a81678c9773c6c613f2ce1812f74cf6439b
INTURI SANDHYA <inturisandhya9@gmail.com>
2025-07-29 11:39:46 +0530

    Add files via upload

---
+++ Autoboxing1.java
@@ -0,0 +1,11 @@
+package oops_skill;
+import java.util.ArrayList;
+public class Autoboxing1 {
+     public static void main(String[] args) {
+    	 ArrayList<Integer> list = new ArrayList<>();
+    	 list.add(1);
+    	 list.add(2);
+    	 list.add(3);
+    	 System.out.println("ArrayList:" + list);
+     }
+}
---
+++ unboxing.java
@@ -0,0 +1,11 @@
+package oops_skill;
+import java.util.ArrayList;
+public class unboxing {
+	 public static void main(String[] args) {
+	        Integer obj = Integer.valueOf(10); 
+	        int x = obj; 
+
+	        System.out.println("object creation/cached object " + obj);
+	        System.out.println("unboxing " + x);
+	 }
+}
